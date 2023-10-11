472c5c5c9293bd949e0d58fed7e277821771ab12
Furkan Cingöz <efcingoz@outlook.com>
2023-10-11 15:39:54 +0300

    Initial Commit

---
+++ kullanici etkileşimi/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  kullanici etkileşimi
+//
+//  Created by Furkan Cingöz on 11.10.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
